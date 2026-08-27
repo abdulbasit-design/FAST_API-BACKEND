569711ea53922278a4e719abf19486fc261f332f
Abdul Basit <abdul.basit@tekdexinc.com>
2026-08-27 19:42:28 +0500

    implemented authorization factor for user and admin

--- app/routes/todo.py
+++ app/routes/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.schemas.todo import Todo
 from app.database.connection import todos_collection
 from app.utils.auth import get_current_user, require_admin
@@ -24,6 +24,8 @@ def create_todo(
 
     todo_data = todo.model_dump()
 
+    todo_data["owner_id"] = current_user["user_id"]
+
     todos_collection.insert_one(todo_data)
 
     return {"message": "Todo is Added"}
@@ -33,22 +35,48 @@ def create_todo(
 def get_todos(
     current_user=Depends(get_current_user)
 ):
-    todos = todos_collection.find({}, {"_id": 0})
+
+    if current_user["role"] == "admin":
+        todos = todos_collection.find(
+            {},
+            {"_id": 0}
+        )
+
+    else:
+        todos = todos_collection.find(
+            {
+                "owner_id": current_user["user_id"]
+            },
+            {"_id": 0}
+        )
 
     return list(todos)
 
 
 @router.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
+def get_todo(
+    todo_id: int,
+    current_user=Depends(get_current_user)
+):
+
     todo = todos_collection.find_one(
         {"id": todo_id},
         {"_id": 0}
     )
 
-    if todo:
+    if not todo:
+        return {"error": "Todo not found"}
+
+    if current_user["role"] == "admin":
         return todo
 
-    return {"error": "Todo not found"}
+    if todo["owner_id"] != current_user["user_id"]:
+        raise HTTPException(
+            status_code=403,
+            detail="You are not allowed to access this Todo"
+        )
+
+    return todo
 
 
 @router.put("/todos/{todo_id}")
@@ -58,15 +86,27 @@ def update_todo(
     current_user=Depends(get_current_user)
 ):
 
+    todo = todos_collection.find_one({
+        "id": todo_id
+    })
+
+    if not todo:
+        return {"error": "Todo not found"}
+
+    if current_user["role"] != "admin":
+
+        if todo["owner_id"] != current_user["user_id"]:
+            raise HTTPException(
+                status_code=403,
+                detail="You are not allowed to update this Todo"
+            )
+
     todo_data = updated_todo.model_dump()
 
     result = todos_collection.update_one(
         {"id": todo_id},
         {"$set": todo_data}
     )
-
-    if result.matched_count == 0:
-        return {"error": "Todo not found"}
 
     return {"message": "Todo Updated"}
 
@@ -77,11 +117,23 @@ def delete_todo(
     current_user=Depends(get_current_user)
 ):
 
-    result = todos_collection.delete_one(
-        {"id": todo_id}
-    )
+    todo = todos_collection.find_one({
+        "id": todo_id
+    })
 
-    if result.deleted_count == 0:
+    if not todo:
         return {"error": "Todo not found"}
+
+    if current_user["role"] != "admin":
+
+        if todo["owner_id"] != current_user["user_id"]:
+            raise HTTPException(
+                status_code=403,
+                detail="You are not allowed to delete this Todo"
+            )
+
+    result = todos_collection.delete_one({
+        "id": todo_id
+    })
 
     return {"message": "Todo Deleted"}
